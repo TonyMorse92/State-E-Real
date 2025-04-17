11254301702e9ca14261f7cb327feb415874249b
TonyMorse92 <anthonymorse92@gmail.com>
2025-04-17 18:52:06 -0400

    Making houses.

--- simulation.py
+++ simulation.py
@@ -1,7 +1,14 @@
-k = []
+import random as rand
 
-for i in range(0,23):
-	k.append(i** + 3)
+class House:
+
+	def __init__(self, price, num_bedrooms, num_stories):
+		self.price = price
+		self.num_bedrooms = num_bedrooms
+		self.num_stories = num_stories
 
 
-print(k)	
+for _ in range(0,10):
+	t = House(rand.uniform(100_000,150_000), rand.choice([1,2,4]), rand.choice([1,2]))
+
+	print(f"The price is: ${t.price}\nThere are {t.num_bedrooms} bedroom(s)\nAnd it is a {t.num_stories} story house.\n")
