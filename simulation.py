4edff7189d8410fd972a7220b1bd0a937e22c971
TonyMorse92 <anthonymorse92@gmail.com>
2025-04-29 12:39:49 -0400

    Added more samples to loop.

--- simulation.py
+++ simulation.py
@@ -8,7 +8,7 @@ class House:
 		self.num_stories = num_stories
 
 
-for _ in range(0,100):
+for _ in range(0,10000):
 	home = House(rand.uniform(100_000,150_000), rand.choice([1,2,4]), rand.choice([1,2]))
 
 	print(f"The price is: ${home.price}\nThere are {home.num_bedrooms} bedroom(s)\nAnd it is a {home.num_stories} story house.\n")
