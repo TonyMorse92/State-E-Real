9f0fb6b1f3b7e08196bd2f1408f558910240aad9
TonyMorse92 <anthonymorse92@gmail.com>
2025-04-29 12:42:22 -0400

    Updating sim.

--- simulation.py
+++ simulation.py
@@ -7,8 +7,18 @@ class House:
 		self.num_bedrooms = num_bedrooms
 		self.num_stories = num_stories
 
+homes = []
 
 for _ in range(0,10000):
 	home = House(rand.uniform(100_000,150_000), rand.choice([1,2,4]), rand.choice([1,2]))
 
-	print(f"The price is: ${home.price}\nThere are {home.num_bedrooms} bedroom(s)\nAnd it is a {home.num_stories} story house.\n")
+	#print(f"The price is: ${home.price}\nThere are {home.num_bedrooms} bedroom(s)\nAnd it is a {home.num_stories} story house.\n")
+	homes.append(home)
+
+highest_price = 0
+
+for home in homes:
+	if home.price > highest_price:
+		highest_price = home.price
+
+print(f"The highest price is: {highest_price}")
